b78e05e9b979a554a8cdb1e71b3c347d60f22fff
Eric <eric@android>
2018-04-23 18:48:07 +1000

    tweaks to timer, navbar colour and background settings

--- lifeclock/js/timer.js
+++ lifeclock/js/timer.js
@@ -11,18 +11,19 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365));
+  var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in an element with id="timer"
-  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  document.getElementById("timer").innerHTML = years + "y " + days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "IT'S HERE!";
+    document.getElementById("demo").innerHTML = "Your Still Here...";
   }
 }, 1000);
