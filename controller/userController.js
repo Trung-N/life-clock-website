27dcab5695aa8841c19718876589e772d6e36687
zvoo <zvoo@student.unimelb.edu.au>
2018-05-09 12:44:49 +0800

    revert changes

--- controller/userController.js
+++ controller/userController.js
@@ -201,7 +201,7 @@ module.exports.postlogin = passport.authenticate('local-login', {
 });
 
 module.exports.postsignup = passport.authenticate('local-signup', {
-    successRedirect : '/home', // redirect to the secure profile section
+    successRedirect : '/login-test', // redirect to the secure profile section
     failureRedirect : '/signup', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
 });
